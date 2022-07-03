5b74c09485c005f1c1928d4293fc1a42c5cfa985
InKyoJeong <viapolar@naver.com>
2022-07-03 22:15:58 +0900

    chore: prettier, eslint 환경 설정
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const root = createRoot(document.getElementById("root") as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
