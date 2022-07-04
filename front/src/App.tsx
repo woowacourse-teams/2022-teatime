f905638e1c925380bb4ad7fbdfd607c258571f80
InKyoJeong <viapolar@naver.com>
2022-07-04 19:24:35 +0900

    feat: useFetch hook 구현
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '@pages/Home';
+
 const App = () => {
   return (
-    <div>
-      <h1>티타임</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
