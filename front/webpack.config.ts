1d630c24e01a325ecfeee93ae73d90aa8e9acf84
InKyoJeong <viapolar@naver.com>
2022-07-04 13:21:25 +0900

    chore: 환경 설정 수정 - eslint rule추가 - alias 설정
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

--- front/webpack.config.ts
+++ front/webpack.config.ts
@@ -1,46 +1,51 @@
-import path from "path";
-import webpack from "webpack";
-import "webpack-dev-server";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'path';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const config: webpack.Configuration = {
-  mode: isDevelopment ? "development" : "production",
+  mode: isDevelopment ? 'development' : 'production',
   entry: {
-    app: "./src/index.tsx",
+    app: './src/index.tsx',
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+    },
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
           presets: [
-            "@babel/preset-env",
-            ["@babel/preset-react", { runtime: "automatic" }],
-            "@babel/preset-typescript",
+            '@babel/preset-env',
+            ['@babel/preset-react', { runtime: 'automatic' }],
+            '@babel/preset-typescript',
           ],
         },
-        exclude: path.join(__dirname, "node_modules"),
+        exclude: path.join(__dirname, 'node_modules'),
       },
       {
         test: /\.css?$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html",
-      favicon: "public/favicon.ico",
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico',
     }),
   ],
   devServer: {
