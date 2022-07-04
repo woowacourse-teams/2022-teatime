4c2775d2994c8b0a19e3c026f1e64627a78965d5
InKyoJeong <viapolar@naver.com>
2022-07-04 19:23:05 +0900

    feat: msw 코치 리스트 모킹
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

---
+++ front/src/mocks/data.ts
@@ -0,0 +1,11 @@
+const coaches = [
+  {
+    id: 0,
+    name: '준',
+    description: '밝음의 아이콘 준입니다.',
+    image:
+      'https://yt3.ggpht.com/tQY2QAJsgYflRlt2hyvrNFiDSb1b9Cq_ivSEhm9RiavVGZbQVoJYMMh_eLjQmofNOWMsuNUTvQ=s176-c-k-c0x00ffffff-no-rj',
+  },
+];
+
+export { coaches };
