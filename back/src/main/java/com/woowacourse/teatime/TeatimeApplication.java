95cf882712645d5e4c4fb41e8280f1fb85eb7e25
pup-paw <wldusdl0310@gmail.com>
2022-07-04 18:40:06 +0900

    chore: 백엔드 환경 설정 및 gitignore 추가

---
+++ back/src/main/java/com/woowacourse/teatime/TeatimeApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.teatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeatimeApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/woowacourse/teatime/TeatimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.teatime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
