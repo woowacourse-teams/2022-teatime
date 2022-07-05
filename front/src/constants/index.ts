8f834ab1668866768b8e55bfb9d63deeb7270b90
InKyoJeong <viapolar@naver.com>
2022-07-05 11:54:49 +0900

    feat: 코치 카드 컴포넌트 구현
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

---
+++ front/src/constants/index.ts
@@ -0,0 +1,8 @@
+const COACH_COLORS = [
+  '#76bae8, #e8bdf0',
+  'Violet, Orange',
+  'yellow, lightGreen',
+  'lightCyan, skyBlue, deepSkyBlue',
+];
+
+export { COACH_COLORS };
---
+++ front/src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export { HomeContainer };
