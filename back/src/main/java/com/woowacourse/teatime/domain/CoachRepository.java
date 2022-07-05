d1777683e04d0b74f5a79e26fd47fc45ccee9cd8
yeongunheo <gjduddns@gmail.com>
2022-07-05 20:35:44 +0900

    feat: DB에 저장된 코치 목록 조회 기능 구현

---
+++ back/src/main/java/com/woowacourse/teatime/domain/CoachRepository.java
@@ -0,0 +1,7 @@
+package com.woowacourse.teatime.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoachRepository extends JpaRepository<Coach, Long> {
+
+}
