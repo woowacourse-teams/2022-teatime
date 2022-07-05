664ba0bb8226cb05f8f6e4904c3d899806ddf0b3
yeongunheo <gjduddns@gmail.com>
2022-07-05 22:10:49 +0900

    feat: 저장된 코치 목록 조회 서비스 구현

---
+++ back/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into coach(name, description, image) values ('준', '밝음의 아이콘 준 :웃음:', 'https://avatars.githubusercontent.com/u/50367798?v=4');
+insert into coach(name, description, image) values ('포코', '포초리 맞을 사람~:기쁨:', 'https://avatars.githubusercontent.com/u/81607552?v=4');
