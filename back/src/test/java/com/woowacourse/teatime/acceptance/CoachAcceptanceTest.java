b97802967041a3c6715b68bae00a17e46e96022d
yeongunheo <gjduddns@gmail.com>
2022-07-06 15:40:24 +0900

    feat: 코치 목록 조회 api 구현

---
+++ back/src/test/java/com/woowacourse/teatime/acceptance/CoachAcceptanceTest.java
@@ -0,0 +1,35 @@
+package com.woowacourse.teatime.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.woowacourse.teatime.CoachResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class CoachAcceptanceTest extends AcceptanceTest{
+
+    @DisplayName("코치 목록을 조회한다.")
+    @Test
+    void findAll() {
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/api/coaches")
+                .then().log().all()
+                .extract();
+
+        List<CoachResponse> result = new ArrayList<>(response.jsonPath().getList(".", CoachResponse.class));
+
+        assertAll(
+                () -> assertThat(result.size()).isEqualTo(2),
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+        );
+    }
+}
