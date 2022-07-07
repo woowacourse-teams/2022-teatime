dfc7b3e6264b7da43498e6e6312439b35049eb56
InKyoJeong <viapolar@naver.com>
2022-07-08 03:42:14 +0900

    feat: 헤더 로고 추가
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,7 +1,16 @@
-import { HeaderContainer } from './styles';
+import LogoIcon from '@assets/logo.png';
+import { ROUTES } from '@constants/index';
+import { HeaderContainer, LogoLink } from './styles';
 
 const Header = () => {
-  return <HeaderContainer>티타임</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <LogoLink to={ROUTES.HOME}>
+        <img src={LogoIcon} />
+        <h1>티타임</h1>
+      </LogoLink>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
--- front/src/components/Header/styles.ts
+++ front/src/components/Header/styles.ts
@@ -1,9 +1,31 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
   height: 50px;
-  box-shadow: 0px 4px 4px #ececec;
+  padding: 0 10%;
+  box-shadow: 0px 3px 4px #ececec;
+
+  h1 {
+    font-size: 18px;
+  }
+
+  img {
+    width: 30px;
+    margin-right: 8px;
+  }
 `;
 
-export { HeaderContainer };
+const LogoLink = styled(Link)`
+  display: flex;
+  color: black;
+  align-items: center;
+  flex-direction: row;
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+export { HeaderContainer, LogoLink };
