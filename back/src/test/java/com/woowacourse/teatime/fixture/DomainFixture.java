a74d1c235bdfcaaf874550e92ab8fb3b5bd6215a
pup-paw <wldusdl0310@gmail.com>
2022-07-18 14:53:44 +0900

    feat: 예약을 승인하면 승인된 상태로 변경하는 도메인 기능 구현

--- back/src/test/java/com/woowacourse/teatime/fixture/DomainFixture.java
+++ back/src/test/java/com/woowacourse/teatime/fixture/DomainFixture.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import com.woowacourse.teatime.domain.Coach;
+import com.woowacourse.teatime.domain.Crew;
 
 public class DomainFixture {
 
@@ -13,4 +14,6 @@ public class DomainFixture {
     public static final LocalDate LOCAL_DATE = LocalDate.of(2022, 7, 18);
     public static final LocalTime LOCAL_TIME = LocalTime.MIN;
     public static final LocalDateTime DATE_TIME = LocalDateTime.of(LOCAL_DATE, LOCAL_TIME);
+
+    public static final Crew CREW = new Crew();
 }
