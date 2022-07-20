3955175d3b9c4ef9b04ffcbbc9fd2cd0298caaf8
Maru <60432062+chawani@users.noreply.github.com>
2022-07-20 17:16:54 +0900

    fix: PathVariable 타입이 일치하지 않을 때 예외를 핸들링 하도록 수정 (#74)

--- back/src/main/java/com/woowacourse/teatime/exception/ControllerAdvice.java
+++ back/src/main/java/com/woowacourse/teatime/exception/ControllerAdvice.java
@@ -15,6 +15,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
@@ -48,6 +49,11 @@ public class ControllerAdvice {
         FieldError mainError = fieldErrors.get(0);
 
         return ResponseEntity.badRequest().body(new ErrorResponse(mainError.getDefaultMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidPathVariable(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler({
