c78b752ffed8a3c7586012bb42a634a966cb9faf
pup-paw <wldusdl0310@gmail.com>
2022-07-21 14:40:36 +0900

    refactor: Dto 변환 관련 로직을 Domain에서 Dto로 이동

--- back/src/main/java/com/woowacourse/teatime/controller/dto/ScheduleResponse.java
+++ back/src/main/java/com/woowacourse/teatime/controller/dto/ScheduleResponse.java
@@ -1,6 +1,8 @@
 package com.woowacourse.teatime.controller.dto;
 
 import com.woowacourse.teatime.domain.Schedule;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,7 +19,31 @@ public class ScheduleResponse {
         this.schedules = schedules;
     }
 
-    public static ScheduleResponse of(int day, List<Schedule> schedules) {
+    public static List<ScheduleResponse> from(List<Schedule> schedules) {
+        List<Integer> days = findDays(schedules);
+        List<ScheduleResponse> scheduleResponses = new ArrayList<>();
+        for (Integer day : days) {
+            List<Schedule> scheduleByDay = findByDay(day, schedules);
+            scheduleResponses.add(ScheduleResponse.of(day, scheduleByDay));
+        }
+        return scheduleResponses;
+    }
+
+    private static List<Integer> findDays(List<Schedule> schedules) {
+        return schedules.stream()
+                .map(Schedule::getLocalDateTime)
+                .map(LocalDateTime::getDayOfMonth)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private static List<Schedule> findByDay(int day, List<Schedule> schedules) {
+        return schedules.stream()
+                .filter(schedule -> schedule.isSameDay(day))
+                .collect(Collectors.toList());
+    }
+
+    private static ScheduleResponse of(int day, List<Schedule> schedules) {
         List<ScheduleDto> scheduleDtos = schedules.stream()
                 .map(ScheduleDto::new)
                 .collect(Collectors.toList());
--- back/src/main/java/com/woowacourse/teatime/service/ScheduleService.java
+++ back/src/main/java/com/woowacourse/teatime/service/ScheduleService.java
@@ -5,14 +5,12 @@ import com.woowacourse.teatime.controller.dto.ScheduleResponse;
 import com.woowacourse.teatime.controller.dto.ScheduleUpdateRequest;
 import com.woowacourse.teatime.domain.Coach;
 import com.woowacourse.teatime.domain.Schedule;
-import com.woowacourse.teatime.domain.Schedules;
 import com.woowacourse.teatime.exception.NotFoundCoachException;
 import com.woowacourse.teatime.repository.CoachRepository;
 import com.woowacourse.teatime.repository.ScheduleRepository;
 import com.woowacourse.teatime.util.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -40,30 +38,15 @@ public class ScheduleService {
 
         LocalDateTime start = Date.findFirstDay(request.getYear(), request.getMonth());
         LocalDateTime end = Date.findLastDay(request.getYear(), request.getMonth());
-        Schedules schedules = getSchedules(coachId, start, end);
-
-        return getScheduleResponses(schedules, schedules.findDays());
+        List<Schedule> schedules
+                = scheduleRepository.findByCoachIdAndLocalDateTimeBetweenOrderByLocalDateTime(coachId, start, end);
+        return ScheduleResponse.from(schedules);
     }
 
     private void validateCoachId(Long id) {
         if (!coachRepository.existsById(id)) {
             throw new NotFoundCoachException();
         }
-    }
-
-    private Schedules getSchedules(Long coachId, LocalDateTime start, LocalDateTime end) {
-        List<Schedule> schedules
-                = scheduleRepository.findByCoachIdAndLocalDateTimeBetweenOrderByLocalDateTime(coachId, start, end);
-        return new Schedules(schedules);
-    }
-
-    private List<ScheduleResponse> getScheduleResponses(Schedules schedules, List<Integer> days) {
-        List<ScheduleResponse> scheduleResponses = new ArrayList<>();
-        for (Integer day : days) {
-            List<Schedule> schedulesByDay = schedules.findByDay(day);
-            scheduleResponses.add(ScheduleResponse.of(day, schedulesByDay));
-        }
-        return scheduleResponses;
     }
 
     public void update(Long coachId, ScheduleUpdateRequest request) {
