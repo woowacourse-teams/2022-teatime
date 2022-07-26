e6786275ee580d601987ad665987b453b931488e
pup-paw <wldusdl0310@gmail.com>
2022-07-26 22:45:23 +0900

    feat: 사전 질문에 대한 크루의 응답을 담는 Sheet 생성

---
+++ back/src/main/java/com/woowacourse/teatime/domain/Sheet.java
@@ -0,0 +1,35 @@
+package com.woowacourse.teatime.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Sheet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Reservation reservation;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(nullable = false)
+    private String questionContent;
+
+    @Lob
+    private String answerContent;
+}
