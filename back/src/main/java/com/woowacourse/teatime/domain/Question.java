dd363c4d71970c00f04dbc5502fb18084e8a5284
pup-paw <wldusdl0310@gmail.com>
2022-07-26 21:53:37 +0900

    feat: 코치의 사전 질문을 담는 Question 생성

---
+++ back/src/main/java/com/woowacourse/teatime/domain/Question.java
@@ -0,0 +1,31 @@
+package com.woowacourse.teatime.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Coach coach;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(nullable = false)
+    private String content;
+}
