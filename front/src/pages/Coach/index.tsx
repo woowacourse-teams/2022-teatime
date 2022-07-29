afac5ed307fc97b78e9b37b3b107dfb3a58775fd
InKyoJeong <viapolar@naver.com>
2022-07-29 16:42:25 +0900

    feat: Board 스타일 추가

--- front/src/pages/Coach/index.tsx
+++ front/src/pages/Coach/index.tsx
@@ -1,16 +1,62 @@
 import { useNavigate } from 'react-router-dom';
-import { Layout } from './styles';
+import BoardItem from '@components/BoardItem';
+import useFetch from '@hooks/useFetch';
+import { Crew } from '@typings/domain';
+import * as S from './styles';
 
 const Coach = () => {
   const navigate = useNavigate();
+  const { data: crews } = useFetch<Crew[], null>('/api/crews');
+
   const handleClickCard = () => {
     navigate(`/schedule/41`);
   };
 
   return (
-    <Layout>
-      <button onClick={handleClickCard}>일정 추가</button>
-    </Layout>
+    <S.Layout>
+      <div style={{ height: 50, display: 'flex', justifyContent: 'flex-end' }}>
+        <button onClick={handleClickCard}>일정 추가</button>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <S.Board>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              width: '100%',
+              borderBottom: '3px solid #FFA500',
+              paddingBottom: 15,
+              marginBottom: 15,
+            }}
+          >
+            <div
+              style={{
+                width: 8,
+                height: 8,
+                backgroundColor: '#FFA500',
+                borderRadius: 5,
+                marginRight: 10,
+              }}
+            />
+            <span style={{ fontWeight: 'bold' }}>대기중인 일정</span>
+          </div>
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              overflow: 'scroll',
+              padding: '0 10px',
+            }}
+          >
+            {crews?.map((crew) => {
+              return <BoardItem key={crew.id} buttonName="승인하기" {...crew} />;
+            })}
+          </div>
+        </S.Board>
+        <S.Board></S.Board>
+        <S.Board></S.Board>
+      </div>
+    </S.Layout>
   );
 };
 
--- front/src/pages/Coach/styles.ts
+++ front/src/pages/Coach/styles.ts
@@ -3,14 +3,19 @@ import styled from 'styled-components';
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 1000px;
+  max-width: 1080px;
   margin: 50px auto;
 `;
 
 const Board = styled.div`
-  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 330px;
   height: calc(100vh - 200px);
+  padding: 15px;
   background-color: #f5f5f5;
+  border-radius: 15px;
 `;
 
 export { Layout, Board };
