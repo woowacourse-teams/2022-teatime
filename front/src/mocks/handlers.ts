e51f135c036b6fdd47407d9f76790d85df896763
InKyoJeong <viapolar@naver.com>
2022-07-29 16:41:40 +0900

    fix: msw handler baseurl 추가

--- front/src/mocks/handlers.ts
+++ front/src/mocks/handlers.ts
@@ -1,29 +1,30 @@
+import { rest } from 'msw';
 import type {
   DaySchedule as DayScheduleResponse,
   Coach as CoachResponse,
   Crew as CrewResponse,
 } from '@typings/domain';
-import { rest } from 'msw';
+import { BASE_URL } from '@api/index';
 import { coachList, crewList, scheduleList } from './data';
 
 const handlers = [
-  rest.get<CoachResponse[]>('/api/coaches', (req, res, ctx) => {
+  rest.get<CoachResponse[]>(`${BASE_URL}/api/coaches`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(coachList));
   }),
 
-  rest.get<DayScheduleResponse[]>('/api/coaches/:id/schedules', (req, res, ctx) => {
+  rest.get<DayScheduleResponse[]>(`${BASE_URL}/api/coaches/:id/schedules`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(scheduleList));
   }),
 
-  rest.post('/api/coaches/:id/schedules/:scheduleId', (req, res, ctx) => {
+  rest.post(`${BASE_URL}/api/coaches/:id/schedules/:scheduleId`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 
-  rest.put('/api/coaches/:id/schedules', (req, res, ctx) => {
+  rest.put(`${BASE_URL}/api/coaches/:id/schedules`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 
-  rest.get<CrewResponse[]>('/api/crews', (req, res, ctx) => {
+  rest.get<CrewResponse[]>(`${BASE_URL}/api/crews`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(crewList));
   }),
 ];
