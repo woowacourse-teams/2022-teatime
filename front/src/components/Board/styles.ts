7b9cfa9420ace1354cbe604e9b02476f4df73183
InKyoJeong <viapolar@naver.com>
2022-07-30 15:54:07 +0900

    feat: Board 컴포넌트 구현

---
+++ front/src/components/Board/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const BoardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 330px;
+  height: calc(100vh - 200px);
+  padding: 15px;
+  background-color: #f5f5f5;
+  border-radius: 15px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  border-bottom: 3px solid #ffa500;
+  padding-bottom: 15px;
+  margin-bottom: 15px;
+
+  div {
+    width: 8px;
+    height: 8px;
+    background-color: #ffa500;
+    border-radius: 5px;
+    margin-right: 10px;
+  }
+
+  span {
+    font-weight: bold;
+  }
+`;
+
+const ScrollContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+  padding: 0 10px;
+`;
+
+export { BoardContainer, TitleContainer, ScrollContainer };
