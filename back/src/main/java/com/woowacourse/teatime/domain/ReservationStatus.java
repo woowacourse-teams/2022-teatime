6a92bf3ca818855fc6fd5affb19540580cd268d3
pup-paw <wldusdl0310@gmail.com>
2022-08-02 17:29:24 +0900

    refactor: 면담 상태에 DONE(완료됨) 추가

--- back/src/main/java/com/woowacourse/teatime/domain/ReservationStatus.java
+++ back/src/main/java/com/woowacourse/teatime/domain/ReservationStatus.java
@@ -3,7 +3,8 @@ package com.woowacourse.teatime.domain;
 public enum ReservationStatus {
     BEFORE_APPROVED,
     APPROVED,
-    IN_PROGRESS;
+    IN_PROGRESS,
+    DONE;
 
     public static boolean isBeforeApproved(ReservationStatus status) {
         return status.equals(BEFORE_APPROVED);
