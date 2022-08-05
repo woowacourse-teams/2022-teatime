f99410612180124033502d5948cc1a457330fbd5
Youngwoo Yoo <63579113+Yboyu0u@users.noreply.github.com>
2022-08-05 13:20:56 +0900

    [BE] Tomcat시간을 KST 설정 취소 (#226)
    * fix: tomcat 시간을 KST로 설정
    
    * fix: tomcat 시간을 KST로 설정 취소

--- back/src/main/java/com/woowacourse/teatime/TeatimeApplication.java
+++ back/src/main/java/com/woowacourse/teatime/TeatimeApplication.java
@@ -1,7 +1,5 @@
 package com.woowacourse.teatime;
 
-import java.util.TimeZone;
-import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,10 +8,5 @@ public class TeatimeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeatimeApplication.class, args);
-    }
-
-    @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
