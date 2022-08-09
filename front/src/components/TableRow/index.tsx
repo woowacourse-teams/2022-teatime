1d9db6c9fda995d30de602709a4e28a107a2c835
Ahn <tn3748@naver.com>
2022-08-10 02:16:17 +0900

    feat: 휴지통 아이콘 추가

--- front/src/components/TableRow/index.tsx
+++ front/src/components/TableRow/index.tsx
@@ -2,24 +2,25 @@ import { History } from '@typings/domain';
 import { getMonthDate, getHourMinutes } from '@utils/index';
 import * as S from './styles';
 
-import ScheduleIcon from '@assets/schedule.svg';
+import TrashIcon from '@assets/trash.svg';
 
 interface TableRowProps {
   history: History;
   statusName: string;
   color: string;
   bgColor: string;
+  onClickSheet: (sheetStatus: string, reservationId: number) => void;
 }
 
-const TableRow = ({ history, statusName, color, bgColor }: TableRowProps) => {
-  const { coachName, coachImage, dateTime } = history;
+const TableRow = ({ history, statusName, color, bgColor, onClickSheet }: TableRowProps) => {
+  const { reservationId, status, coachName, coachImage, dateTime } = history;
   const date = getMonthDate(dateTime);
   const time = getHourMinutes(dateTime);
 
   return (
     <tr>
       <td>
-        <S.Span bgColor={bgColor} color={color}>
+        <S.Span bgColor={bgColor} color={color} onClick={() => onClickSheet(status, reservationId)}>
           {statusName}
         </S.Span>
       </td>
@@ -31,10 +32,7 @@ const TableRow = ({ history, statusName, color, bgColor }: TableRowProps) => {
       </td>
       <td>{date}</td>
       <td>{time}</td>
-      <td>
-        <S.Icon src={ScheduleIcon} alt="면담 시트 아이콘" />
-        <S.Icon src={ScheduleIcon} alt="휴지통 아이콘" />
-      </td>
+      <td>{status !== 'IN_PROGRESS' && <S.Icon src={TrashIcon} alt="휴지통 아이콘" />}</td>
     </tr>
   );
 };
