b2b5504827ecfa6f776129a4d239c74888ea5339
Ahn <82227098+jin7969@users.noreply.github.com>
2022-08-15 14:35:35 +0900

    feat: 로그인 api 연결 (#277)
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>

--- front/src/pages/Certification/index.tsx
+++ front/src/pages/Certification/index.tsx
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+
+import api from '@api/index';
 
 const Certification = () => {
   const [searchParams] = useSearchParams();
-
   const code = searchParams.get('code') ?? '';
-  console.log('code', code);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.post('/api/auth/login', {
+          code,
+        });
+        console.log('data', data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
   return <div></div>;
 };
