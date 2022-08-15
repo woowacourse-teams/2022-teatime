c66a606e3773ad361fbb4c698b20859a9049d15e
InKyoJeong <viapolar@naver.com>
2022-08-15 16:52:30 +0900

    fix: slack url 변경

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -3,7 +3,8 @@ import * as S from './styles';
 const Home = () => {
   const handleLogin = () => {
     location.href =
-      'https://slack.com/oauth/v2/authorize?scope=users.profile:read,users:read,users:read.email&user_scope=openid&client_id=3853132979991.3908459157459';
+      // 'https://slack.com/oauth/v2/authorize?scope=users.profile:read,users:read,users:read.email&user_scope=openid&client_id=3853132979991.3908459157459';
+      'https://slack.com/openid/connect/authorize?response_type=code&scope=openid%20profile%20email&client_id=3853132979991.3908459157459&redirect_uri=https://teatime.pe.kr/certification';
   };
 
   return (
