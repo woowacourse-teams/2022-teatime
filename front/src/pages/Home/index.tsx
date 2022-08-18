2e115db34ffc848dfb4106134d0b056eec8f288c
InKyoJeong <viapolar@naver.com>
2022-08-18 13:53:56 +0900

    fix: 재접속시 role에 따른 라우팅 변경

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,11 +1,24 @@
+import { useContext, useEffect } from 'react';
+
+import { UserStateContext } from '@context/UserProvider';
 import * as S from './styles';
 import SlackIcon from '@assets/slack.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { userData } = useContext(UserStateContext);
+
   const handleLogin = () => {
     location.href =
       'https://slack.com/openid/connect/authorize?response_type=code&scope=openid%20profile%20email&client_id=3853132979991.3908459157459&redirect_uri=https://teatime.pe.kr/certification';
   };
+
+  useEffect(() => {
+    if (userData) {
+      navigate(`/${userData.role.toLowerCase()}`);
+    }
+  }, [userData]);
 
   return (
     <S.Login>
