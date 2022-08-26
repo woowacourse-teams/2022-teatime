c79429227d1914f94751020b201e057267a03a39
Ahn <tn3748@naver.com>
2022-08-26 16:46:12 +0900

    refactor: dropdown 로직 수정

--- front/src/components/Dropdown/index.tsx
+++ front/src/components/Dropdown/index.tsx
@@ -1,47 +1,34 @@
-import { useContext, useRef } from 'react';
+import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import useOutsideClick from '@hooks/useOutsideClick';
 import { ROUTES } from '@constants/index';
 import { UserDispatchContext } from '@context/UserProvider';
 import * as S from './styles';
 
 interface DropdownProps {
-  name: string;
-  image: string;
+  dropdownRef: React.RefObject<HTMLUListElement>;
+  isActive: boolean;
+  role?: string;
 }
 
-const Dropdown = ({ name, image }: DropdownProps) => {
+const Dropdown = ({ dropdownRef, isActive, role }: DropdownProps) => {
   const dispatch = useContext(UserDispatchContext);
   const navigate = useNavigate();
-  const dropdownRef = useRef(null);
-  const profileRef = useRef(null);
-  const [isActive, setIsActive] = useOutsideClick(profileRef, dropdownRef, false);
 
-  const toggleDropdown = () => {
-    setIsActive(!isActive);
+  const handleLogout = () => {
+    dispatch({ type: 'DELETE_USER' });
+    navigate(ROUTES.HOME);
   };
 
-  // const handleLogout = () => {
-  //   dispatch({ type: 'DELETE_USER' });
-  //   navigate(ROUTES.HOME);
-  // };
-
   return (
-    <S.ProfileContainer>
-      <S.ProfileWrapper ref={profileRef} onClick={toggleDropdown}>
-        <span>{name}</span>
-        <img src={image} alt="프로필 이미지" />
-      </S.ProfileWrapper>
-      <S.ContentList ref={dropdownRef} isActive={isActive}>
-        <ul>
-          <Link to={ROUTES.CREW_HISTORY}>
-            <li>히스토리</li>
-          </Link>
-          <li>로그아웃</li>
-        </ul>
-      </S.ContentList>
-    </S.ProfileContainer>
+    <S.ContentList ref={dropdownRef} isActive={isActive}>
+      {role === 'CREW' && (
+        <Link to={ROUTES.CREW_HISTORY}>
+          <li>히스토리</li>
+        </Link>
+      )}
+      <li onClick={handleLogout}>로그아웃</li>
+    </S.ContentList>
   );
 };
 
--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,14 +1,22 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 
-import { UserStateContext } from '@context/UserProvider';
 import Dropdown from '@components/Dropdown';
+import useOutsideClick from '@hooks/useOutsideClick';
+import { UserStateContext } from '@context/UserProvider';
 import { ROUTES } from '@constants/index';
+import * as S from './styles';
 
 import LogoIcon from '@assets/logo.svg';
-import * as S from './styles';
 
 const Header = () => {
   const { userData } = useContext(UserStateContext);
+  const dropdownRef = useRef(null);
+  const profileRef = useRef(null);
+  const [isActive, setIsActive] = useOutsideClick(profileRef, dropdownRef, false);
+
+  const toggleDropdown = () => {
+    setIsActive(!isActive);
+  };
 
   return (
     <S.HeaderContainer>
@@ -16,7 +24,17 @@ const Header = () => {
         <S.LogoImage src={LogoIcon} />
         <h1>티타임</h1>
       </S.LogoLink>
-      {userData && <Dropdown name={userData.name} image={userData.image} />}
+      <S.ProfileContainer>
+        {userData && (
+          <S.ProfileWrapper ref={profileRef} onClick={toggleDropdown}>
+            <span>{userData.name}</span>
+            <img src={userData.image} alt="프로필 이미지" />
+          </S.ProfileWrapper>
+        )}
+        {isActive && (
+          <Dropdown dropdownRef={dropdownRef} isActive={isActive} role={userData?.role} />
+        )}
+      </S.ProfileContainer>
     </S.HeaderContainer>
   );
 };
