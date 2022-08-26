0c131b444847c879f07d7fec7bf7d04dc579975b
Ahn <tn3748@naver.com>
2022-08-27 02:14:11 +0900

    refactor: 헤더 height 수정

--- front/src/components/Header/styles.ts
+++ front/src/components/Header/styles.ts
@@ -6,7 +6,7 @@ const HeaderContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 60px;
+  height: 50px;
   padding: 0 10%;
   box-shadow: 0px 3px 4px ${({ theme }) => theme.colors.GRAY_300};
 
