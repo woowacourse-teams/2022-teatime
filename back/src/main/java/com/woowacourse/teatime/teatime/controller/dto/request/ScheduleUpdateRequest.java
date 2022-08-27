03ddbab0e03dd1e95b84ed7713b7c36df8719612
chawani <chawani@naver.com>
2022-08-28 00:14:20 +0900

    fix: ScheduleUpdateRequest 기본 생성자 추가

--- back/src/main/java/com/woowacourse/teatime/teatime/controller/dto/request/ScheduleUpdateRequest.java
+++ back/src/main/java/com/woowacourse/teatime/teatime/controller/dto/request/ScheduleUpdateRequest.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class ScheduleUpdateRequest {
 
