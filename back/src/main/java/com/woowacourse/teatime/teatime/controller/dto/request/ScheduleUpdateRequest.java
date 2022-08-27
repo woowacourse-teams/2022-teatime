162b2719718c686f75743dbbccf150575df2f654
chawani <chawani@naver.com>
2022-08-27 16:19:35 +0900

    fix: ScheduleUpdateRequest dto 수정

--- back/src/main/java/com/woowacourse/teatime/teatime/controller/dto/request/ScheduleUpdateRequest.java
+++ back/src/main/java/com/woowacourse/teatime/teatime/controller/dto/request/ScheduleUpdateRequest.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class ScheduleUpdateRequest {
 
