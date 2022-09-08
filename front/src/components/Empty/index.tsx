32ee5d246ead1bc1972f328c20f5366e739a874b
InKyoJeong <viapolar@naver.com>
2022-09-09 05:34:53 +0900

    feat: empty 컴포넌트 추가

---
+++ front/src/components/Empty/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles';
+import EmptyImage from '@assets/empty.svg';
+
+interface EmptyProps {
+  text: string;
+}
+
+const Empty = ({ text }: EmptyProps) => {
+  return (
+    <S.Container>
+      <img src={EmptyImage} />
+      <span>{text}</span>
+    </S.Container>
+  );
+};
+
+export default Empty;
