5d1510a16ad647396a55c41a921c88b10d5c6e60
Ahn <82227098+jin7969@users.noreply.github.com>
2022-09-15 00:44:12 +0900

    [FE] 히스토리 빈 내용 추가 (#453)
    * refactor: history 페이지 정렬 변경
    
    * feat: 히스토리가 없을 경우 빈내용 표시
    
    * refactor: table 내부가 아닌 외부에서 빈화면 표시
    
    * refactor: 프로필 왼쪽으로 정렬
    
    Co-authored-by: Koy <viapolar@naver.com>

--- front/src/components/TableRow/styles.ts
+++ front/src/components/TableRow/styles.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
+const TbodyRow = styled.tr`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 2fr repeat(3, 1fr);
+  grid-auto-rows: 96px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.GRAY_300};
+  color: ${({ theme }) => theme.colors.BLUE_700};
+`;
+
 const Span = styled.span<{ color: string; bgColor: string }>`
   padding: 4px 10px;
   font-size: 16px;
@@ -11,7 +20,7 @@ const Span = styled.span<{ color: string; bgColor: string }>`
 const Profile = styled.div`
   display: flex;
   align-items: center;
-  padding: 0 18px;
+  padding-left: 54px;
 
   img {
     width: 48px;
@@ -33,4 +42,4 @@ const Icon = styled.img`
   }
 `;
 
-export { Span, Profile, Icon };
+export { TbodyRow, Span, Profile, Icon };
