02ada6d502b308152c4c6e64ed1fcb535cc0f2ca
Ahn <tn3748@naver.com>
2022-09-15 19:38:16 +0900

    fix: slack url 수정

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -12,7 +12,7 @@ const Home = () => {
 
   const handleLogin = () => {
     location.href =
-      'https://woowacourse.slack.com/oauth/v2/authorize?client_id=3853132979991.3908459157459&scope=&user_scope=openid%2Cemail%2Cprofile&redirect_uri=https%3A%2F%2Fdev.teatime.pe.kr%2Fcertification';
+      'https://slack.com/oauth/v2/authorize?client_id=3853132979991.3908459157459&scope=&user_scope=openid%2Cemail%2Cprofile&redirect_uri=https%3A%2F%2Fdev.teatime.pe.kr%2Fcertification';
   };
 
   useEffect(() => {
