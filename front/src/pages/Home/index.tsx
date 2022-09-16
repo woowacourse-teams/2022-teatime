13bd0dee3d94e101dab6e5e27b03b0bd8e86d5fc
Koy <viapolar@naver.com>
2022-09-16 15:35:23 +0900

    fix: 슬랙 url 수정 (#474)

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -12,7 +12,7 @@ const Home = () => {
 
   const handleLogin = () => {
     location.href =
-      'https://slack.com/oauth/v2/authorize?client_id=3853132979991.3908459157459&scope=&user_scope=openid%2Cemail%2Cprofile&redirect_uri=https%3A%2F%2Fdev.teatime.pe.kr%2Fcertification';
+      'https://slack.com/openid/connect/authorize?scope=openid,email,profile&response_type=code&redirect_uri=https://dev.teatime.pe.kr/certification&client_id=3853132979991.3908459157459';
   };
 
   useEffect(() => {
