7e22af6c602e59535d352cecf6438567f6ba0445
Ahn <82227098+jin7969@users.noreply.github.com>
2022-09-16 16:08:56 +0900

    feat: 히스토리 시트가 없을 경우 빈 화면 표시 (#475)

--- front/src/pages/HistorySheet/index.tsx
+++ front/src/pages/HistorySheet/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { AxiosError } from 'axios';
 
 import Frame from '@components/Frame';
@@ -7,12 +7,11 @@ import Sheet from '@components/Sheet';
 import BackButton from '@components/BackButton';
 import HistoryItem from '@components/HistoryItem';
 import { getCrewHistoriesByCoach } from '@api/crew';
-import { ROUTES } from '@constants/index';
 import type { HistoryList } from '@typings/domain';
 import * as S from '@styles/common';
+import EmptyContent from '@components/EmptyContent';
 
 const HistorySheet = () => {
-  const navigate = useNavigate();
   const { id: crewId } = useParams();
   const [historyIndex, setHistoryIndex] = useState(0);
   const [historyList, setHistoryList] = useState<HistoryList[]>();
@@ -35,33 +34,31 @@ const HistorySheet = () => {
     })();
   }, []);
 
-  if (!historyList) return <></>;
-
-  if (!historyList.length) {
-    alert('아직 히스토리가 없습니다.');
-    navigate(ROUTES.COACH);
-    return;
-  }
-
   return (
     <Frame>
-      <S.InfoContainer>
-        {historyList.map((history, index) => {
-          const { reservationId, coachImage, coachName, dateTime } = history;
-          return (
-            <HistoryItem
-              key={reservationId}
-              index={index}
-              image={coachImage}
-              name={coachName}
-              dateTime={dateTime}
-              onClick={handleClickHistoryItem}
-              historyIndex={historyIndex}
-            />
-          );
-        })}
-      </S.InfoContainer>
-      <Sheet title="작성한 면담 내용" sheets={historyList[historyIndex].sheets} isView />
+      {!historyList?.length ? (
+        <EmptyContent text={`현재 히스토리가 없습니다.`} />
+      ) : (
+        <>
+          <S.InfoContainer>
+            {historyList.map((history, index) => {
+              const { reservationId, coachImage, coachName, dateTime } = history;
+              return (
+                <HistoryItem
+                  key={reservationId}
+                  index={index}
+                  image={coachImage}
+                  name={coachName}
+                  dateTime={dateTime}
+                  onClick={handleClickHistoryItem}
+                  historyIndex={historyIndex}
+                />
+              );
+            })}
+          </S.InfoContainer>
+          <Sheet title="작성한 면담 내용" sheets={historyList[historyIndex].sheets} isView />
+        </>
+      )}
       <BackButton />
     </Frame>
   );
