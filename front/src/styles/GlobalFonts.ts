f951f2fbfb9a9e913910250fc325116c1c7ae586
Koy <viapolar@naver.com>
2022-09-19 14:37:25 +0900

    refactor: 폰트 최적화 (#495)

--- front/src/styles/GlobalFonts.ts
+++ front/src/styles/GlobalFonts.ts
@@ -6,6 +6,7 @@ const GlobalFonts = createGlobalStyle`
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff');
     font-weight: normal;
     font-style: normal;
+    font-display: swap;
     }
 `;
 
