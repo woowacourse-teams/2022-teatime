e6904838a64ec11e36b26cc966a855c4548af345
Ahn <tn3748@naver.com>
2022-09-20 14:39:59 +0900

    feat: 작성이 불가능한 상태일 때 제출하기 버튼 스타일 추가

--- front/src/components/Sheet/styles.ts
+++ front/src/components/Sheet/styles.ts
@@ -1,27 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const SheetContainer = styled.div`
   width: 100%;
   overflow: scroll;
   ::-webkit-scrollbar {
     display: none;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-
-  button {
-    margin: 0 20px;
-    padding: 10px 20px;
-    border-radius: 20px;
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer;
-    :hover {
-      opacity: 0.7;
-    }
   }
 `;
 
@@ -35,6 +18,36 @@ const SecondButton = styled.button`
   background-color: ${({ theme }) => theme.colors.BLUE_600};
   color: ${({ theme }) => theme.colors.WHITE};
   border: none;
+`;
+
+const ButtonContainer = styled.div<{ isUnalterable: boolean }>`
+  display: flex;
+  justify-content: center;
+
+  button {
+    margin: 0 20px;
+    padding: 10px 20px;
+    border-radius: 20px;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+
+    :hover {
+      opacity: 0.7;
+    }
+
+    ${(props) =>
+      props.isUnalterable &&
+      css`
+        background-color: ${({ theme }) => theme.colors.GRAY_200};
+        color: ${({ theme }) => theme.colors.GRAY_600};
+        border: none;
+
+        :hover {
+          opacity: 1;
+        }
+      `}
+  }
 `;
 
 export { SheetContainer, ButtonContainer, FirstButton, SecondButton };
