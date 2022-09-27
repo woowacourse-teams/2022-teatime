7dfcd4a3d970db4a07c88941c147f660adb73164
Yeongun Heo <52141636+yeongunheo@users.noreply.github.com>
2022-09-27 14:06:37 +0900

    [BE] Request 마다 쿼리 개수를 로깅하는 기능 구현 (#526)
    * feat: Request 마다 쿼리 개수를 로깅하는 기능 구현
    
    * feat: 요청이 처리되고 응답되는데 까지 걸리는 시간을 로깅하는 기능 구현
    
    * feat: local 또는 dev 환경일 때만 쿼리수를 로깅하도록 수정
    
    * fix: counter가 null일 경우 쿼리수를 증가하지 않도록 수정
    
    * refactor: 롬복 적용
    
    * refactor: HibernateConfig에 롬복 적용

---
+++ back/src/main/java/com/woowacourse/teatime/teatime/aspect/QueryCountInspector.java
@@ -0,0 +1,47 @@
+package com.woowacourse.teatime.teatime.aspect;
+
+import lombok.Getter;
+import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryCountInspector implements StatementInspector {
+
+    private final ThreadLocal<Counter> queryCount = new ThreadLocal<>();
+
+    public void startCounter() {
+        queryCount.set(new Counter(0L, System.currentTimeMillis()));
+    }
+
+    public Counter getQueryCount() {
+        return queryCount.get();
+    }
+
+    public void clearCounter() {
+        queryCount.remove();
+    }
+
+    @Override
+    public String inspect(String sql) {
+        Counter counter = queryCount.get();
+        if (counter != null) {
+            counter.increaseCount();
+        }
+        return sql;
+    }
+
+    @Getter
+    class Counter {
+        private Long count;
+        private Long time;
+
+        public Counter(Long count, Long time) {
+            this.count = count;
+            this.time = time;
+        }
+
+        public void increaseCount() {
+            count++;
+        }
+    }
+}
--- back/src/main/java/com/woowacourse/teatime/teatime/config/WebConfig.java
+++ back/src/main/java/com/woowacourse/teatime/teatime/config/WebConfig.java
@@ -4,11 +4,14 @@ import com.google.common.net.HttpHeaders;
 import com.woowacourse.teatime.auth.support.CoachArgumentResolver;
 import com.woowacourse.teatime.auth.support.CrewArgumentResolver;
 import com.woowacourse.teatime.auth.support.UserArgumentResolver;
+import com.woowacourse.teatime.teatime.aspect.LoggingInterceptor;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -19,6 +22,9 @@ public class WebConfig implements WebMvcConfigurer {
     private final CoachArgumentResolver coachArgumentResolver;
     private final CrewArgumentResolver crewArgumentResolver;
     private final UserArgumentResolver userArgumentResolver;
+    private final LoggingInterceptor loggingInterceptor;
+
+    private final Environment environment;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
@@ -31,6 +37,17 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .exposedHeaders(HttpHeaders.LOCATION);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        for (String profileName : environment.getActiveProfiles()) {
+            if (profileName.equals("local") || profileName.equals("dev")) {
+                registry.addInterceptor(loggingInterceptor);
+            }
+        }
+
+        WebMvcConfigurer.super.addInterceptors(registry);
     }
 
     @Override
