12a91d302244bd6747c9bb3fe4da0e98919b429c
Maru <60432062+chawani@users.noreply.github.com>
2022-09-29 14:50:27 +0900

    feat: 코치 자신의 프로필을 조회하는 기능 구현 (#545)

---
+++ back/src/main/java/com/woowacourse/teatime/teatime/controller/dto/response/CoachProfileResponse.java
@@ -0,0 +1,22 @@
+package com.woowacourse.teatime.teatime.controller.dto.response;
+
+
+import com.woowacourse.teatime.teatime.domain.Coach;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CoachProfileResponse {
+
+    private String name;
+    private String description;
+    private String image;
+
+    public CoachProfileResponse(Coach coach) {
+        this(coach.getName(), coach.getDescription(), coach.getImage());
+    }
+}
