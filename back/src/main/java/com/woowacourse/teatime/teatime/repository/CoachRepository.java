ecac3ca44e396be75cbcef89690fced182f6de67
yeongunheo <gjduddns@gmail.com>
2022-10-01 22:33:13 +0900

    refactor: 크루 메인뷰에서 발생하는 1+N 문제 해결

--- back/src/main/java/com/woowacourse/teatime/teatime/repository/CoachRepository.java
+++ back/src/main/java/com/woowacourse/teatime/teatime/repository/CoachRepository.java
@@ -1,11 +1,21 @@
 package com.woowacourse.teatime.teatime.repository;
 
 import com.woowacourse.teatime.teatime.domain.Coach;
+import com.woowacourse.teatime.teatime.repository.dto.CoachWithPossible;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CoachRepository extends JpaRepository<Coach, Long> {
 
     Optional<Coach> findByEmail(String email);
 
+    @Query(value = "SELECT c.id AS id, c.name AS name, c.description AS description, c.image AS image, EXISTS ("
+            + "SELECT * FROM schedule s2 WHERE s2.coach_id = id AND s2.is_possible = TRUE) AS possible "
+            + "FROM coach c "
+            + "LEFT JOIN schedule s "
+            + "ON s.coach_id = c.id "
+            + "GROUP BY c.id", nativeQuery = true)
+    List<CoachWithPossible> findCoaches();
 }
--- back/src/test/java/com/woowacourse/teatime/teatime/repository/CoachRepositoryTest.java
+++ back/src/test/java/com/woowacourse/teatime/teatime/repository/CoachRepositoryTest.java
@@ -1,9 +1,15 @@
 package com.woowacourse.teatime.teatime.repository;
 
+import static com.woowacourse.teatime.teatime.fixture.DomainFixture.getCoachJason;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.woowacourse.teatime.teatime.domain.Coach;
+import com.woowacourse.teatime.teatime.domain.Schedule;
 import com.woowacourse.teatime.teatime.fixture.DomainFixture;
+import com.woowacourse.teatime.teatime.repository.dto.CoachWithPossible;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +21,9 @@ public class CoachRepositoryTest {
 
     @Autowired
     private CoachRepository coachRepository;
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
 
     @DisplayName("전체 코치 목록을 조회한다.")
     @Test
@@ -51,5 +60,64 @@ public class CoachRepositoryTest {
         Optional<Coach> coach = coachRepository.findByEmail(DomainFixture.COACH_JASON.getEmail());
 
         assertThat(coach).isEmpty();
+    }
+
+    @DisplayName("코치의 등록된 스케줄이 하나도 없을 경우 isPossible이 false이다.")
+    @Test
+    void findCoaches1() {
+        // given
+        Coach coach = coachRepository.save(getCoachJason());
+
+        // when
+        List<CoachWithPossible> coaches = coachRepository.findCoaches();
+
+        // then
+        assertAll(
+                () -> assertThat(coaches).hasSize(1),
+                () -> assertThat(coaches.get(0).getId()).isEqualTo(coach.getId()),
+                () -> assertThat(coaches.get(0).getName()).isEqualTo(coach.getName()),
+                () -> assertThat(coaches.get(0).getDescription()).isEqualTo(coach.getDescription()),
+                () -> assertThat(coaches.get(0).getImage()).isEqualTo(coach.getImage()),
+                () -> assertThat(coaches.get(0).getPossible()).isFalse()
+        );
+    }
+
+    @DisplayName("코치의 스케줄이 예약된 경우 isPossible이 false이다.")
+    @Test
+    void findCoaches2() {
+        // given
+        Coach coach = coachRepository.save(getCoachJason());
+        Schedule schedule = scheduleRepository.save(new Schedule(coach, LocalDateTime.now()));
+        schedule.reserve();
+
+        // when
+        List<CoachWithPossible> coaches = coachRepository.findCoaches();
+
+        // then
+        assertAll(
+                () -> assertThat(coaches).hasSize(1),
+                () -> assertThat(coaches.get(0).getName()).isEqualTo(coach.getName()),
+                () -> assertThat(coaches.get(0).getPossible()).isFalse()
+        );
+    }
+
+    @DisplayName("코치의 등록된 스케줄 중 예약가능한 일정이 하나라도 있다면 true이다.")
+    @Test
+    void findCoaches3() {
+        // given
+        Coach coach = coachRepository.save(getCoachJason());
+        Schedule schedule1 = scheduleRepository.save(new Schedule(coach, LocalDateTime.now()));
+        Schedule schedule2 = scheduleRepository.save(new Schedule(coach, LocalDateTime.now()));
+        schedule1.reserve();
+
+        // when
+        List<CoachWithPossible> coaches = coachRepository.findCoaches();
+
+        // then
+        assertAll(
+                () -> assertThat(coaches).hasSize(1),
+                () -> assertThat(coaches.get(0).getName()).isEqualTo(coach.getName()),
+                () -> assertThat(coaches.get(0).getPossible()).isTrue()
+        );
     }
 }
