1bbbc699ea60b419c99604988f5a4e3dbd93dedf
yeongunheo <gjduddns@gmail.com>
2022-10-02 20:22:22 +0900

    fix: 코치목록을 조회할 때 현재시간이후의 예약가능한 일정이 존재할 때만 isPossible이 true이도록 수정

--- back/src/test/java/com/woowacourse/teatime/teatime/repository/CoachRepositoryTest.java
+++ back/src/test/java/com/woowacourse/teatime/teatime/repository/CoachRepositoryTest.java
@@ -87,7 +87,8 @@ public class CoachRepositoryTest {
     void findCoaches2() {
         // given
         Coach coach = coachRepository.save(getCoachJason());
-        Schedule schedule = scheduleRepository.save(new Schedule(coach, LocalDateTime.now()));
+        LocalDateTime tomorrow = LocalDateTime.now().plusDays(1);
+        Schedule schedule = scheduleRepository.save(new Schedule(coach, tomorrow));
         schedule.reserve();
 
         // when
@@ -101,13 +102,14 @@ public class CoachRepositoryTest {
         );
     }
 
-    @DisplayName("코치의 등록된 스케줄 중 예약가능한 일정이 하나라도 있다면 true이다.")
+    @DisplayName("코치의 등록된 스케줄 중 예약가능한 일정이 하나라도 있다면 isPossible이 true이다.")
     @Test
     void findCoaches3() {
         // given
         Coach coach = coachRepository.save(getCoachJason());
-        Schedule schedule1 = scheduleRepository.save(new Schedule(coach, LocalDateTime.now()));
-        Schedule schedule2 = scheduleRepository.save(new Schedule(coach, LocalDateTime.now()));
+        LocalDateTime tomorrow = LocalDateTime.now().plusMonths(1);
+        Schedule schedule1 = scheduleRepository.save(new Schedule(coach, tomorrow));
+        Schedule schedule2 = scheduleRepository.save(new Schedule(coach, tomorrow.plusHours(1)));
         schedule1.reserve();
 
         // when
@@ -118,6 +120,49 @@ public class CoachRepositoryTest {
                 () -> assertThat(coaches).hasSize(1),
                 () -> assertThat(coaches.get(0).getName()).isEqualTo(coach.getName()),
                 () -> assertThat(coaches.get(0).getPossible()).isTrue()
+        );
+    }
+
+    @DisplayName("코치의 등록된 스케줄 중 예약가능하지만 지난 일정만 존재한다면 isPossible이 false이다.")
+    @Test
+    void findCoaches4() {
+        // given
+        Coach coach = coachRepository.save(getCoachJason());
+        LocalDateTime before = LocalDateTime.now().minusMinutes(1);
+        Schedule schedule = scheduleRepository.save(new Schedule(coach, before));
+
+        // when
+        List<CoachWithPossible> coaches = coachRepository.findCoaches();
+
+        // then
+        assertAll(
+                () -> assertThat(coaches).hasSize(1),
+                () -> assertThat(coaches.get(0).getName()).isEqualTo(coach.getName()),
+                () -> assertThat(coaches.get(0).getPossible()).isFalse()
+        );
+    }
+
+    @DisplayName("코치의 등록된 스케줄 중 지난일정과 예약이 완료된 일정만 존재한다면 isPossible이 false이다.")
+    @Test
+    void findCoaches5() {
+        // given
+        Coach coach = coachRepository.save(getCoachJason());
+        LocalDateTime before1 = LocalDateTime.now().minusMinutes(1);
+        LocalDateTime before2 = LocalDateTime.now().minusMinutes(1);
+        LocalDateTime tomorrow = LocalDateTime.now().plusDays(1);
+        Schedule schedule1 = scheduleRepository.save(new Schedule(coach, before1));
+        Schedule schedule2 = scheduleRepository.save(new Schedule(coach, before2));
+        Schedule schedule3 = scheduleRepository.save(new Schedule(coach, tomorrow));
+        schedule3.reserve();
+
+        // when
+        List<CoachWithPossible> coaches = coachRepository.findCoaches();
+
+        // then
+        assertAll(
+                () -> assertThat(coaches).hasSize(1),
+                () -> assertThat(coaches.get(0).getName()).isEqualTo(coach.getName()),
+                () -> assertThat(coaches.get(0).getPossible()).isFalse()
         );
     }
 }
