29500fee4785fcddc824c57c9f589d6b01cfd379
pup-paw <wldusdl0310@gmail.com>
2022-10-02 14:52:33 +0900

    feat: 시트 제출시 알람이 가는 기능 추가

--- back/src/main/java/com/woowacourse/teatime/teatime/service/AlarmTitle.java
+++ back/src/main/java/com/woowacourse/teatime/teatime/service/AlarmTitle.java
@@ -2,11 +2,12 @@ package com.woowacourse.teatime.teatime.service;
 
 public enum AlarmTitle {
 
-    APPLY("☕️티타임이 신청되었습니다.", "#CCE6BA"),
-    CONFIRM("✅티타임이 확정되었습니다.", "#B4E0FF"),
-    CANCEL("🥹아이쿠...! 티타임이 취소되었습니다..", "#E6ACA8"),
-    REMIND_CREW("⏰내일 티타임이 있습니다. 시트를 잊지 말고 제출해주세요!!", "#FFE594"),
-    REMIND_COACH("⏰내일 티타임이 있습니다.", "#FF2594");
+    APPLY("☕️ 티타임이 신청되었습니다.", "#CCE6BA"),
+    CONFIRM("✅ 티타임이 확정되었습니다.", "#B4E0FF"),
+    SUBMIT_SHEET("📝 시트가 제출되었습니다.", "#9498FF"),
+    CANCEL("🥹 아이쿠...! 티타임이 취소되었습니다..", "#E6ACA8"),
+    REMIND_CREW("⏰ 내일 티타임이 있습니다. 시트를 잊지 말고 제출해주세요!!", "#FFE594"),
+    REMIND_COACH("⏰ 내일 티타임이 있습니다.", "#FFE594");
 
     private final String message;
     private final String barColor;
