b94d840347543fdadfb5245e29b30bbe5f6bebbb
youngwooyoo <fbduddn97@naver.com>
2022-10-04 20:59:20 +0900

    fix: 크루 메인 뷰 n+1 문제 해결

---
+++ back/src/main/java/com/woowacourse/teatime/teatime/repository/dto/CoachWithPossible.java
@@ -0,0 +1,18 @@
+package com.woowacourse.teatime.teatime.repository.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CoachWithPossible {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+    private Boolean possible;
+}
--- back/src/main/java/com/woowacourse/teatime/teatime/service/CoachService.java
+++ back/src/main/java/com/woowacourse/teatime/teatime/service/CoachService.java
@@ -8,7 +8,7 @@ import com.woowacourse.teatime.teatime.domain.Coach;
 import com.woowacourse.teatime.teatime.exception.NotFoundCoachException;
 import com.woowacourse.teatime.teatime.repository.CoachRepository;
 import com.woowacourse.teatime.teatime.repository.ScheduleRepository;
-import java.util.LinkedList;
+import com.woowacourse.teatime.teatime.repository.dto.CoachWithPossible;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,13 +24,8 @@ public class CoachService {
 
     @Transactional(readOnly = true)
     public List<CoachFindResponse> findAll() {
-        List<CoachFindResponse> response = new LinkedList<>();
-        List<Coach> coaches = coachRepository.findAll();
-        for (Coach coach : coaches) {
-            boolean isPossible = scheduleRepository.existsIsPossibleByCoachId(coach.getId());
-            response.add(new CoachFindResponse(coach, isPossible));
-        }
-        return response;
+        List<CoachWithPossible> coachWithPossibles = coachRepository.findCoaches();
+        return CoachFindResponse.of(coachWithPossibles);
     }
 
     public Long save(CoachSaveRequest request) {
