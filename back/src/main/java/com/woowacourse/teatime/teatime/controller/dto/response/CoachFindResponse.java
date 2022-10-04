bdcdf5ea07a7f48adca8eb698badbee96d3e35e4
yeongunheo <gjduddns@gmail.com>
2022-10-04 18:20:08 +0900

    fix: 크루메인뷰에서 발생하는 에러 수정

--- back/src/main/java/com/woowacourse/teatime/teatime/controller/dto/response/CoachFindResponse.java
+++ back/src/main/java/com/woowacourse/teatime/teatime/controller/dto/response/CoachFindResponse.java
@@ -1,9 +1,6 @@
 package com.woowacourse.teatime.teatime.controller.dto.response;
 
 import com.woowacourse.teatime.teatime.domain.Coach;
-import com.woowacourse.teatime.teatime.repository.dto.CoachWithPossible;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,12 +19,5 @@ public class CoachFindResponse {
 
     public CoachFindResponse(Coach coach, boolean isPossible) {
         this(coach.getId(), coach.getName(), coach.getDescription(), coach.getImage(), isPossible);
-    }
-
-    public static List<CoachFindResponse> of(List<CoachWithPossible> coachWithPossibles) {
-        return coachWithPossibles.stream()
-                .map(c -> new CoachFindResponse(c.getId(), c.getName(), c.getDescription(), c.getImage(),
-                        c.getPossible()))
-                .collect(Collectors.toList());
     }
 }
