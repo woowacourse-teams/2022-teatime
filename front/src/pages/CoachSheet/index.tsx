23a4cf42220661b47a975f2867b7b9434ef34cd2
Ahn <tn3748@naver.com>
2022-10-06 16:30:27 +0900

    refactor: 면담 완료 버튼 삭제

--- front/src/pages/CoachSheet/index.tsx
+++ front/src/pages/CoachSheet/index.tsx
@@ -1,47 +1,27 @@
-import { useContext, useEffect, useState } from 'react';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
 import { AxiosError } from 'axios';
 
 import Frame from '@components/Frame';
 import ReservationInfo from '@components/ReservationInfo';
 import Sheet from '@components/Sheet';
 import BackButton from '@components/BackButton';
-import { SnackbarContext } from '@context/SnackbarProvider';
-import { completeReservation } from '@api/reservation';
 import { getCrewReservationByCoach } from '@api/crew';
-import { ROUTES } from '@constants/index';
 import type { ReservationByCoach } from '@typings/domain';
 import * as S from '@styles/common';
-import * as Styled from './styles';
 
 interface LocationState {
   state: {
     crewId: number;
-    hasCompleteButton?: boolean;
   };
 }
 
 const CoachSheet = () => {
-  const navigate = useNavigate();
-  const showSnackbar = useContext(SnackbarContext);
   const { id: reservationId } = useParams();
   const {
-    state: { crewId, hasCompleteButton },
+    state: { crewId },
   } = useLocation() as LocationState;
   const [reservationInfo, setReservationInfo] = useState<ReservationByCoach>();
-
-  const handleCompleteReservation = async () => {
-    if (!confirm('면담을 완료하시겠습니까?')) return;
-
-    try {
-      await completeReservation(reservationId as string);
-      showSnackbar({ message: '완료되었습니다. ✅' });
-      navigate(ROUTES.COACH, { replace: true });
-    } catch (error) {
-      alert(error);
-      console.log(error);
-    }
-  };
 
   useEffect(() => {
     (async () => {
@@ -68,14 +48,7 @@ const CoachSheet = () => {
           dateTime={reservationInfo.dateTime}
         />
       </S.InfoContainer>
-      <Styled.SheetContainer>
-        <Sheet title="작성한 면담 내용" sheets={reservationInfo.sheets} isReadOnly />
-        {hasCompleteButton && (
-          <Styled.CompleteButtonWrapper>
-            <button onClick={handleCompleteReservation}>면담 완료하기</button>
-          </Styled.CompleteButtonWrapper>
-        )}
-      </Styled.SheetContainer>
+      <Sheet title="작성한 면담 내용" sheets={reservationInfo.sheets} isReadOnly />
       <BackButton />
     </Frame>
   );
