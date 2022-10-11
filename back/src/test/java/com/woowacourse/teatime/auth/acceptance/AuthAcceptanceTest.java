dcd0d1b1e019fc724ac90615b15da53a4baf3acf
chawani <chawani@naver.com>
2022-10-11 13:14:20 +0900

    test: 토큰 재발급 테스트 코드 작성

---
+++ back/src/test/java/com/woowacourse/teatime/auth/acceptance/AuthAcceptanceTest.java
@@ -0,0 +1,58 @@
+package com.woowacourse.teatime.auth.acceptance;
+
+import static com.woowacourse.teatime.teatime.domain.Role.CREW;
+import static com.woowacourse.teatime.teatime.fixture.DtoFixture.CREW_SAVE_REQUEST;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+import com.woowacourse.teatime.auth.domain.UserAuthInfo;
+import com.woowacourse.teatime.auth.service.UserAuthService;
+import com.woowacourse.teatime.teatime.acceptance.AcceptanceTestSupporter;
+import com.woowacourse.teatime.teatime.service.CrewService;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class AuthAcceptanceTest extends AcceptanceTestSupporter {
+
+    @Autowired
+    private CrewService crewService;
+    @Autowired
+    private UserAuthService userAuthService;
+
+    private String accessToken;
+    private String refreshToken;
+
+    @BeforeEach
+    void setUp() {
+        Long crewId = crewService.save(CREW_SAVE_REQUEST);
+        accessToken = 크루의_토큰을_발급한다(crewId);
+        refreshToken = UUID.randomUUID().toString();
+        userAuthService.save(new UserAuthInfo(refreshToken, crewId, CREW.name()));
+    }
+
+    @DisplayName("토큰을 재발급한다.")
+    @Test
+    void generateToken() {
+        ExtractableResponse<Response> response = RestAssured.given(super.spec).log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", "Bearer " + accessToken)
+                .cookie("refreshToken", refreshToken)
+                .filter(document("generate-new-token"))
+                .when().get("/api/auth/refresh-token")
+                .then().log().all()
+                .extract();
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+        );
+    }
+}
--- back/src/test/java/com/woowacourse/teatime/auth/repository/UserAuthInfoRepositoryTest.java
+++ back/src/test/java/com/woowacourse/teatime/auth/repository/UserAuthInfoRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.woowacourse.teatime.auth.domain.UserAuthInfo;
 import java.util.Optional;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,14 +15,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 class UserAuthInfoRepositoryTest {
 
     @Autowired
-    UserAuthInfoRepository userAuthInfoRepository;
+    private UserAuthInfoRepository userAuthInfoRepository;
+    private final String refreshToken = UUID.randomUUID().toString();
+    ;
+
+    @BeforeEach
+    void init() {
+        UserAuthInfo token = new UserAuthInfo(refreshToken, 1L, COACH.name());
+        userAuthInfoRepository.save(token);
+    }
 
     @Test
     void save() {
-        String refreshToken = UUID.randomUUID().toString();
-        UserAuthInfo token = new UserAuthInfo(refreshToken, 1L, COACH.name());
+        String refreshToken2 = UUID.randomUUID().toString();
+        UserAuthInfo token = new UserAuthInfo(refreshToken2, 2L, COACH.name());
         userAuthInfoRepository.save(token);
 
+        assertThat(userAuthInfoRepository.findById(refreshToken2).isPresent()).isTrue();
+    }
+
+    @Test
+    void find() {
         Optional<UserAuthInfo> saved = userAuthInfoRepository.findById(refreshToken);
 
         assertThat(saved.isPresent()).isTrue();
