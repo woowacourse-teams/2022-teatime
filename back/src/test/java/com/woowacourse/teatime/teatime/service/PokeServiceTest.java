fda9f590a65d343f649027d2d5f9b8b9d719ae7d
Yeongun Heo <52141636+yeongunheo@users.noreply.github.com>
2022-10-14 20:10:26 +0900

    [BE] 콕 찔러보기 검증로직 추가 (#685)
    * feat: 콕 찔러보기 검증로직 추가
    
    * refactor: 로그 에러레벨 수정
    
    * feat: 커스텀 예외 추가
    
    * refactor: 콕 찔러보기 이미지 url 수정
    
    * refactor: 콕 찔러보기 이미지 url 수정

--- back/src/test/java/com/woowacourse/teatime/teatime/service/PokeServiceTest.java
+++ back/src/test/java/com/woowacourse/teatime/teatime/service/PokeServiceTest.java
@@ -2,12 +2,14 @@ package com.woowacourse.teatime.teatime.service;
 
 import static com.woowacourse.teatime.teatime.fixture.DomainFixture.getCoachJason;
 import static com.woowacourse.teatime.teatime.fixture.DomainFixture.getCrew;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.woowacourse.teatime.teatime.controller.dto.request.PokeSaveRequest;
 import com.woowacourse.teatime.teatime.domain.Coach;
 import com.woowacourse.teatime.teatime.domain.Crew;
 import com.woowacourse.teatime.teatime.domain.Poke;
+import com.woowacourse.teatime.teatime.exception.CannotPokeException;
 import com.woowacourse.teatime.teatime.repository.CoachRepository;
 import com.woowacourse.teatime.teatime.repository.CrewRepository;
 import com.woowacourse.teatime.teatime.repository.PokeRepository;
@@ -48,5 +50,21 @@ class PokeServiceTest {
         // then
         Optional<Poke> actual = pokeRepository.findById(pokeId);
         assertTrue(actual.isPresent());
+    }
+
+    @DisplayName("티타임을 요청을 거부한 코치에게 티타임요청을 보내면 에러를 발생시킼다")
+    @Test
+    void save_false() {
+        // given
+        Crew crew = crewRepository.save(getCrew());
+        Coach coach = coachRepository.save(getCoachJason());
+        coach.modifyProfile(coach.getName(), coach.getDescription(), false);
+
+        // when
+        PokeSaveRequest request = new PokeSaveRequest(coach.getId());
+
+        // then
+        assertThatThrownBy(() -> pokeService.save(crew.getId(), request))
+                .isInstanceOf(CannotPokeException.class);
     }
 }
