10e384dfbc3fab9e0bbd94e946a603b3f916045b
Ahn <tn3748@naver.com>
2022-10-19 11:06:27 +0900

    feat: invite 페이지 로직 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -23,6 +23,7 @@ const CrewHistory = lazy(() => import('@pages/CrewHistory'));
 const CoachHistory = lazy(() => import('@pages/CoachHistory'));
 const CoachProfile = lazy(() => import('@pages/CoachProfile'));
 const DemoHome = lazy(() => import('@pages/DemoHome'));
+const DemoLogin = lazy(() => import('@pages/DemoLogin'));
 
 const App = () => {
   return (
@@ -31,24 +32,20 @@ const App = () => {
         <Header />
         <Suspense fallback={<Loading text="Loading..." />}>
           <Routes>
-            <Route path={'/'} element={<DemoHome />} />
+            <Route path={'/'} element={<DemoLogin />} />
+            <Route path={'/demo-home'} element={<DemoHome />} />
             <Route path={ROUTES.CERTIFICATION} element={<Certification />} />
             <Route path="/*" element={<NotFound />} />
-            <Route element={<AuthRoute role="CREW" />}>
-              <Route path={ROUTES.CREW} element={<CrewMain />} />
-              <Route path={ROUTES.CREW_HISTORY} element={<CrewHistory />} />
-              <Route path={`${ROUTES.RESERVATION}/:id`} element={<Reservation />} />
-              <Route path={`${ROUTES.CREW_SHEET}/:id`} element={<CrewSheet />} />
-            </Route>
-            <Route element={<AuthRoute role="COACH" />}>
-              <Route path={ROUTES.COACH} element={<CoachMain />} />
-              <Route path={ROUTES.SCHEDULE} element={<Schedule />} />
-              <Route path={ROUTES.QUESTION} element={<Question />} />
-              <Route path={ROUTES.COACH_HISTORY} element={<CoachHistory />} />
-              <Route path={ROUTES.COACH_PROFILE} element={<CoachProfile />} />
-              <Route path={`${ROUTES.COACH_SHEET}/:id`} element={<CoachSheet />} />
-              {/* <Route path={`${ROUTES.HISTORY_SHEET}/:id`} element={<HistorySheet />} /> */}
-            </Route>
+            <Route path={ROUTES.CREW} element={<CrewMain />} />
+            <Route path={ROUTES.CREW_HISTORY} element={<CrewHistory />} />
+            <Route path={`${ROUTES.RESERVATION}/:id`} element={<Reservation />} />
+            <Route path={`${ROUTES.CREW_SHEET}/:id`} element={<CrewSheet />} />
+            <Route path={ROUTES.COACH} element={<CoachMain />} />
+            <Route path={ROUTES.SCHEDULE} element={<Schedule />} />
+            <Route path={ROUTES.QUESTION} element={<Question />} />
+            <Route path={ROUTES.COACH_HISTORY} element={<CoachHistory />} />
+            <Route path={ROUTES.COACH_PROFILE} element={<CoachProfile />} />
+            <Route path={`${ROUTES.COACH_SHEET}/:id`} element={<CoachSheet />} />
           </Routes>
         </Suspense>
       </SnackbarProvider>
--- front/src/pages/DemoHome/index.tsx
+++ front/src/pages/DemoHome/index.tsx
@@ -1,10 +1,31 @@
-import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { AxiosError } from 'axios';
 
+import { UserDispatchContext } from '@context/UserProvider';
+import { api } from '@api/index';
 import * as S from './styles';
-import { ROUTES } from '@constants/index';
 
 const DemoHome = () => {
   const navigate = useNavigate();
+  const dispatch = useContext(UserDispatchContext);
+  const { state: nickname } = useLocation();
+
+  const handleUserButton = async (role: string) => {
+    try {
+      const { data: userData } = await api.post('/api/auth/login/v2', {
+        name: nickname,
+        role: role.toUpperCase(),
+      });
+      dispatch({ type: 'SET_USER', userData });
+      navigate(`/${role}`, { replace: true });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        alert(error.response?.data?.message);
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <S.Container>
@@ -21,8 +42,8 @@ const DemoHome = () => {
           <p>티타임은 면담 예약을 편리하게 도와주는 서비스입니다.</p>
         </S.Description>
         <S.ButtonContainer>
-          <S.MentorButton onClick={() => navigate(ROUTES.COACH)}>멘토 체험하기</S.MentorButton>
-          <S.MenteeButton onClick={() => navigate(ROUTES.CREW)}>멘티 체험하기</S.MenteeButton>
+          <S.MentorButton onClick={() => handleUserButton('coach')}>멘토 체험하기</S.MentorButton>
+          <S.MenteeButton onClick={() => handleUserButton('crew')}>멘티 체험하기</S.MenteeButton>
         </S.ButtonContainer>
       </S.MainSection>
     </S.Container>
