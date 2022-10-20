76c698e9cf2a1b360557cdbaf1a00188db03b56c
pup-paw <wldusdl0310@gmail.com>
2022-10-20 11:09:06 +0900

    refactor: 불필요한 테스트 삭제

--- back/src/test/java/com/woowacourse/teatime/auth/controller/AuthControllerTest.java
+++ back/src/test/java/com/woowacourse/teatime/auth/controller/AuthControllerTest.java
@@ -2,13 +2,11 @@ package com.woowacourse.teatime.auth.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.woowacourse.teatime.auth.controller.dto.GenerateTokenDto;
 import com.woowacourse.teatime.teatime.controller.ControllerTestSupporter;
-import javax.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.ResultActions;
@@ -18,7 +16,7 @@ class AuthControllerTest extends ControllerTestSupporter {
 
     @DisplayName("토큰 재발급에 성공한다.")
     @Test
-    void refreshToken() throws Exception {
+    void generateToken() throws Exception {
         //given
         String token = "나 크루다.";
         크루의_토큰을_검증한다(token);
@@ -28,30 +26,10 @@ class AuthControllerTest extends ControllerTestSupporter {
 
         //when
         ResultActions perform = mockMvc.perform(get("/api/auth/refresh-token")
-                        .header("Authorization", "Bearer " + token)
-                        .cookie(new Cookie("refreshToken", "refreshToken")))
+                        .header("Authorization", "Bearer " + token))
                 .andDo(print());
 
         //then
         perform.andExpect(status().isOk());
-    }
-
-    @DisplayName("로그아웃 시 토큰을 삭제한다.")
-    @Test
-    void logout() throws Exception {
-        //given
-        String token = "나 크루다.";
-        크루의_토큰을_검증한다(token);
-
-        doNothing().when(userAuthService).deleteToken(any());
-
-        //when
-        ResultActions perform = mockMvc.perform(delete("/api/auth/logout")
-                        .header("Authorization", "Bearer " + token)
-                        .cookie(new Cookie("refreshToken", "refreshToken")))
-                .andDo(print());
-
-        //then
-        perform.andExpect(status().isNoContent());
     }
 }
