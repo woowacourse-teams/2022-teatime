f848a0207c042642ae5a284336dd97928cf72472
Ahn <tn3748@naver.com>
2022-10-29 02:20:30 +0900

    feat: 크루 로직 모킹 수정 및 폴더 분리

---
+++ front/src/mocks/handlers/crewHandler.js
@@ -0,0 +1,56 @@
+import { rest } from 'msw';
+
+import { BASE_URL } from '@api/index';
+import { coaches, schedules, reservationInfo, crewHistories } from '../dummy/crewData';
+
+const crewHandler = [
+  // 코치 목록 조회
+  rest.get(`${BASE_URL}/api/v2/coaches`, (req, res, ctx) => {
+    const token = req.headers.get('Authorization');
+    if (token === undefined) {
+      return res(ctx.status(401));
+    }
+
+    return res(ctx.status(200), ctx.json(coaches));
+  }),
+
+  // 캘린더에서 스케줄 조회
+  rest.get(`${BASE_URL}/api/v2/coaches/:coachId/schedules`, (req, res, ctx) => {
+    const year = req.url.searchParams.get('year');
+    const month = req.url.searchParams.get('month');
+
+    const result = schedules[`${year}-${month}`] || [];
+
+    return res(ctx.status(200), ctx.json(result));
+  }),
+
+  // 히스토리 조회
+  rest.get(`${BASE_URL}/api/v2/crews/me/reservations`, (req, res, ctx) => {
+    const token = req.headers.get('Authorization');
+    if (token === undefined) {
+      return res(ctx.status(401));
+    }
+
+    return res(ctx.status(200), ctx.json(crewHistories));
+  }),
+
+  // 시트 조회
+  rest.get(`${BASE_URL}/api/v2/crews/me/reservations/:reservationId`, (req, res, ctx) => {
+    const { reservationId } = req.params;
+
+    return res(ctx.status(200), ctx.json(reservationInfo[Number(reservationId)]));
+  }),
+
+  // 예약하기
+  rest.post(`${BASE_URL}/api/v2/reservations`, (req, res, ctx) => {
+    const { scheduleId } = req.body;
+
+    if (scheduleId === null) {
+      return res(ctx.status(400));
+    }
+
+    return res(ctx.status(201), ctx.set('Location', '/0'));
+  }),
+];
+
+export default crewHandler;
