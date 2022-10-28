18ef3158d9d0d508c5a0708b87f484fa65438ceb
InKyoJeong <viapolar@naver.com>
2022-10-28 21:00:10 +0900

    fix: 누락된 타입명 변경

--- front/src/context/BoardModeProvider.tsx
+++ front/src/context/BoardModeProvider.tsx
@@ -1,19 +1,19 @@
 import { createContext, useState } from 'react';
 import { BOARD } from '@constants/index';
-import { BoardValue } from '@typings/domain';
+import type { BoardName } from '@typings/domain';
 
 type BoardContextType = (e: React.MouseEvent<HTMLElement>) => void;
 
-export const BoardStateContext = createContext<BoardValue>(BOARD.BEFORE_APPROVED);
+export const BoardStateContext = createContext<BoardName>(BOARD.BEFORE_APPROVED);
 export const BoardChangeContext = createContext<BoardContextType>(() => null);
 
 const BoardProvider = ({ children }: { children: React.ReactNode }) => {
-  const [selectedBoard, setSelectedBoard] = useState<BoardValue>(BOARD.BEFORE_APPROVED);
+  const [selectedBoard, setSelectedBoard] = useState<BoardName>(BOARD.BEFORE_APPROVED);
 
   const changeBoard = (e: React.MouseEvent<HTMLElement>) => {
     const target = e.target as HTMLElement;
     if (target.tagName !== 'LI') return;
-    setSelectedBoard(target.id as BoardValue);
+    setSelectedBoard(target.id as BoardName);
   };
 
   return (
